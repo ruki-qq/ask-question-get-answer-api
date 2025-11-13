8011769604f32114c279cf34944570e237ead170
rukiqq <mitaka@duck.com>
2025-11-13 17:38:05 +0300

    add models to __init__.py in core module

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,7 +1,7 @@
-__all__ = ("Base", "DBTextDateMixin", "db_helper", "settings")
+__all__ = ("Answer", "Base", "DBTextDateMixin", "Question", "db_helper", "settings")
 
 
 from .config import settings
 from .db_helper import db_helper
-from .models import Base
 from .mixins import DBTextDateMixin
+from .models import Answer, Base, Question
