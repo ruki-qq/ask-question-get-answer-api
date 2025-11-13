91a0933d37b2e60f02935b1a884efd7356409ae2
ruki-qq <ruki@duck.com>
2025-11-13 23:11:14 +0300

    set alembic to async mode

--- src/core/config.py
+++ src/core/config.py
@@ -2,11 +2,11 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_PORT: int
-    POSTGRES_PASSWORD: str
-    POSTGRES_USER: str
-    POSTGRES_DB: str
-    POSTGRES_HOST: str
+    DATABASE_PORT: int = "5432"
+    POSTGRES_PASSWORD: str = "herrington"
+    POSTGRES_USER: str = "billy"
+    POSTGRES_DB: str = "qa_dev"
+    POSTGRES_HOST: str = "172.27.32.1"
     api_prefix: str = "/api/v1"
 
     db_echo: bool = True
